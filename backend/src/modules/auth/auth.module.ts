44a3614e1f9050d2d09a1afeeb0e01136016134e
ÐSergey Dziadevich <developersiteweb@gmail.com>
2022-01-07 10:08:51 +0300

    added Authentication; AuthController; AuthModule; AdminModule

--- backend/src/modules/auth/auth.module.ts
+++ backend/src/modules/auth/auth.module.ts
@@ -3,7 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { PassportModule } from '@nestjs/passport';
 import { AdminModule } from '../admin/admin.module';
 import { AuthService } from './services/auth.service';
-import { LocalStrategy } from './services/local.strategy';
+import { LocalStrategy } from './strategies/local.strategy';
 import { AuthController } from './controller/auth.controller';
 
 @Module({
