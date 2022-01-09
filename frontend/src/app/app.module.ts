08cbf51d9293a6a9e9320d8136ccdbdf4686881f
ÐSergey Dziadevich <developersiteweb@gmail.com>
2022-01-09 19:13:55 +0300

    added angular material

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { NotFoundPageComponent } from './components/not-found-page/not-found-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, NotFoundPageComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
