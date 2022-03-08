c0df2610924633b1e5cd1507972eb61b80d8aea3
ÐSergey Dziadevich <developersiteweb@gmail.com>
2022-03-08 12:54:34 +0300

    added Auth Guard logic

--- frontend/src/app/store/admin-auth-store/services/admin-auth.service.ts
+++ frontend/src/app/store/admin-auth-store/services/admin-auth.service.ts
@@ -2,22 +2,30 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { JwtHelperService } from '@auth0/angular-jwt';
-import { map } from 'rxjs/operators';
-import {AuthData} from '../store/admin-auth-reducer';
+import { filter, map } from 'rxjs/operators';
+import { AuthData } from '../store/admin-auth-reducer';
+import { select, Store } from '@ngrx/store';
+import { getAuthData } from '../store/admin-auth-selectors';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AdminAuthService {
+  isAuth$: Observable<boolean> = this.store$.pipe(
+    select(getAuthData),
+    filter((authData) => authData !== undefined),
+    map((authData) => !!authData)
+  );
+
+  isGuest$ = this.isAuth$.pipe(map((isAuth) => !isAuth));
+
   constructor(
     private httpClient: HttpClient,
     private jwtHelperService: JwtHelperService,
+    private store$: Store
   ) {}
 
-  login(body: {
-    login: string;
-    password: string;
-  }): Observable<AuthData> {
+  login(body: { login: string; password: string }): Observable<AuthData> {
     return this.httpClient
       .post<{ accessToken: string }>('http://localhost:3000/auth/login', body)
       .pipe(
