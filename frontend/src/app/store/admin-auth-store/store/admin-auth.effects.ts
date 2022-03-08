e118f70555b6458eb086b8e20c07159ceb20ba36
ÐSergey Dziadevich <developersiteweb@gmail.com>
2022-03-08 13:57:58 +0300

    added listenAuthorizeEffect

--- frontend/src/app/store/admin-auth-store/store/admin-auth.effects.ts
+++ frontend/src/app/store/admin-auth-store/store/admin-auth.effects.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import {fromEvent, of, timer} from 'rxjs';
+import { fromEvent, of, timer } from 'rxjs';
 import {
   switchMap,
   map,
   catchError,
-  delayWhen,
   filter,
   first,
   tap,
+  distinctUntilChanged,
+  skip,
 } from 'rxjs/operators';
 import {
   extractLoginData,
@@ -22,6 +23,7 @@ import { AdminAuthService } from '../services/admin-auth.service';
 import { AuthData } from './admin-auth-reducer';
 import { select, Store } from '@ngrx/store';
 import { isAuth } from './admin-auth-selectors';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AdminAuthEffects {
@@ -69,37 +71,55 @@ export class AdminAuthEffects {
     { dispatch: false }
   );
 
-  extractLoginData$ = createEffect(
-    () =>
-      this.actions$.pipe(
-        ofType(initAdminAuth, extractLoginData),
-        map(() => {
-          const authDataString = localStorage.getItem('authData');
+  extractLoginData$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(initAdminAuth, extractLoginData),
+      map(() => {
+        const authDataString = localStorage.getItem('authData');
 
-          if (!authDataString) {
-            return logoutSuccess();
-          }
+        if (!authDataString) {
+          return logoutSuccess();
+        }
 
-          const authData: AuthData = JSON.parse(authDataString);
+        const authData: AuthData = JSON.parse(authDataString);
 
-          if (authData.exp * 1000 - 10 * 1000 - Date.now() < 0) {
-            return logoutSuccess();
-          }
+        if (authData.exp * 1000 - 10 * 1000 - Date.now() < 0) {
+          return logoutSuccess();
+        }
 
-          return loginSuccess({ authData });
-        })
-      )
+        return loginSuccess({ authData });
+      })
+    )
   );
 
-  listenStorageEffect$ = createEffect(() => this.actions$.pipe(
-    ofType(initAdminAuth),
-    switchMap(() => fromEvent(window, 'storage')),
-    map(() => extractLoginData())
-  ));
+  listenStorageEffect$ = createEffect(() =>
+    this.actions$.pipe(
+      ofType(initAdminAuth),
+      switchMap(() => fromEvent(window, 'storage')),
+      map(() => extractLoginData())
+    )
+  );
+
+  listenAuthorizeEffect$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(initAdminAuth),
+        switchMap(() => this.adminAuthService.isAuth$),
+        distinctUntilChanged(),
+        skip(1),
+        tap((isAuthorized) => {
+          this.router.navigateByUrl(
+            isAuthorized ? '/admin' : '/admin/auth/login'
+          );
+        })
+      ),
+    { dispatch: false }
+  );
 
   constructor(
     private actions$: Actions,
     private adminAuthService: AdminAuthService,
-    private store$: Store
+    private store$: Store,
+    private router: Router
   ) {}
 }
